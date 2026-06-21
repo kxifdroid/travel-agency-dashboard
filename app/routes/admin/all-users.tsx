e67cff9a99ba5e622af05a114238f30a2573b02f
kxifdroid <kxifiit@gmail.com>
2026-06-21 23:42:13 +0530

    dashboard header

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Header from "../../../components/Header";
 
 const AllUsers = () => {
+    const user = {name: 'Kasif'};
     return (
-        <div>AllUsers</div>
+        <main className="dashboard wrapper">
+            <Header
+                title="Manage Users"
+                description="Filter, sort, and access detailed user profiles" />
+            All users Page contents
+
+        </main>
     )
 }
 export default AllUsers
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,7 +3,12 @@ import {Link, NavLink} from "react-router";
 import {sidebarItems} from "~/constants";
 import {cn} from "~/lib/utils";
 
-const NavItems = () => {
+const NavItems = ({ handleClick }: { handleClick?: () => void}) => {
+    const user = {
+        name: 'Adrian',
+        email: 'contact@jsmastery.pro',
+        imageUrl: '/assets/images/david.webp',
+    }
     return (
         <section className="nav-items">
             <Link to='/' className="link-logo">
@@ -17,7 +22,7 @@ const NavItems = () => {
                             {({ isActive }: { isActive: boolean }) => (
                                 <div className={cn('group nav-item', {
                                     'bg-primary-100 !text-white': isActive
-                                })}>
+                                })} onClick={handleClick}>
                                     <img
                                         src={icon}
                                         alt={label}
@@ -29,6 +34,25 @@ const NavItems = () => {
                         </NavLink>
                     ))}
                 </nav>
+                <footer className="nav-footer">
+                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'David'} />
+                    <article>
+                        <h2>{user?.name}</h2>
+                        <p>{user?.email}</p>
+                    </article>
+                    <button
+                        onClick={() => {
+                            console.log('logout')
+                        }}
+                        className="cursor-pointer"
+                    >
+                        <img
+                            src="/assets/icons/logout.svg"
+                            alt="logout"
+                            className="size-6"
+                        />
+                    </button>
+                </footer>
             </div>
         </section>
     )
