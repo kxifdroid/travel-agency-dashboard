2d426ace4a4860ad379537cc8042dd38613f64e9
kxifdroid <kxifiit@gmail.com>
2026-06-23 01:20:33 +0530

    trips card, some auth

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,15 +1,11 @@
 import {Header, StatsCard, TripCard} from "../../../components"
+import {dashboardStats, user, allTrips} from "~/constants";
+
+const {totalUsers, usersJoined, totalTrips, tripsCreated, userRole} = dashboardStats;
 
 const Dashboard = () => {
-    const user = {name: 'Kasif'};
-    const dashboardStats = {
-        totalUsers: 12450,
-        usersJoined: {currentMonth: 218, lastMonth: 176},
-        totalTrips: 3210,
-        tripsCreated: {currentMonth: 150, lastMonth: 250},
-        userRole: {total: 62, currentMonth: 25, lastMonth: 15}
-    }
-    const {totalUsers, usersJoined, totalTrips, tripsCreated, userRole} = dashboardStats;
+
+
     return (
         <main className="dashboard wrapper">
             <Header
@@ -37,7 +33,22 @@ const Dashboard = () => {
                     />
                 </div>
             </ section>
-            <TripCard />
+            <section className="container">
+                <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+                <div className='trip-grid'>
+                    {allTrips.slice(0,4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+                        <TripCard
+                            key={id}
+                            id={id.toString()}
+                            name={name}
+                            imageUrls={imageUrls[0]}
+                            location={itinerary?.[0]?.location??''}
+                            tags={tags}
+                            price={estimatedPrice}
+                        />
+                    ))}
+                </div>
+            </section>
         </main>
     )
 }
